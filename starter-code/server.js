80a80ea9f7e806da3abdfd61b6f45b33384de6bb
DKosmos <Davidakosmos@gmail.com>
2017-06-27 17:54:31 -0700

    adds aboutController.js to index.html and fixes handlebars template syntax

--- starter-code/server.js
+++ starter-code/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = 'postgres://postgres@localhost:3000/kilovolt'; // DONE: Don't forget to set your own conString
+const conString = 'postgres://postgres:M1d@S4eva@localhost:5432/kilovolt'; // DONE: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
